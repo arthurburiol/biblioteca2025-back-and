0df6677b5edf971d4fc34506fa32a6cb33b5c2f9
Arthur Buriol <arthur.bcontas@gmail.com>
2025-04-01 21:55:27 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
+//importa a biblioteca do express
 import express from "express";
 
+//cria a api com express
 const app = express();
 app.use(express.json());
 
-app.get('/teste', (req, res)=>{
-    res.send('Teste ok.');
+//exemplo de requisição
+app.get("/versao", (requisicao, resposta) => {
+    resposta.send("Versão 2025.1");
+} );
+
+//exemplo de requisição com queryParams
+app.get("/quadrado", (requisicao, resposta) => {
+    var n = parseInt(requisicao.query.numero);
+    var q = n * n;
+    resposta.send(q.toString());
+} );
+
+//exemplo de requisição com routeParams
+app.get("/quadrado/:numero", (requisicao, resposta) => {
+    var n = parseInt(requisicao.query.numero);
+    var q = n * n;
+    resposta.send(q.toString());
+} );
+
+//exemplo de requisição com headersParams
+app.get("/cubo", (requisicao, resposta) => {
+    var n = parseInt(requisicao.headers.numero);
+    var q = n * n * n;
+    resposta.send(q.toString());
+} );
+
+//exemplo de requisição com bodyParams
+app.post("/verificaridade", (requisicao, resposta) => {
+    var n = requisicao.body.nome;
+    var i = parseInt(requisicao.body.idade);
+    if (i >= 18) {
+        resposta.send(`${n} é maior de idade`);
+    }
+    else {
+        resposta.send(`${n} é menor de idade`);
+    }
 });
 
-app.listen(3000, ()=>{console.log(`Servidor rodando.`)});
+//exemplo de requisição com bodyParams
+app.post("/verificaridade2", (requisicao, resposta) => {
+    var n = requisicao.body.nome;
+    var i = parseInt(requisicao.body.idade);
+    var m;
+    if (i >= 18) {
+        m = (`Maior de idade`);
+    }
+    else {
+        m = (`Menor de idade`);
+    }
+
+    resposta.json({
+        "nome": n,
+        "idade": i,
+        "mensagem": m
+    })  
+});
+
+//disponibiliza a api na porta 3000
+app.listen(3000);
